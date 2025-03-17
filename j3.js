83d165b7955f018d70d9bcf97f778b69e3b52113
Devam Mistry <devammistry7856@gmail.com>
2025-03-17 22:24:30 +0530

    Delete j3.js

--- j3.js
+++
@@ -1,7 +0,0 @@
-const user={
-    "name":["ABC", "DEF", "GHI"],
-    "age":25,
-    "course":["FSD", "DE", "FSD2"],
-    "address":["T1", "T2", {"t3":"give again"}]
-}
-console.log(user.address[2].t3);
