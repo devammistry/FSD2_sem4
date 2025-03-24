80313fa181bc0e8a28ec5c9880d3eebca134b533
Devam Mistry <devammistry7856@gmail.com>
2025-03-24 20:47:23 +0530

    Delete day32.js

--- day32.js
+++
@@ -1,6 +0,0 @@
-var ps=require("fs");
-ps.mkdirSync("day3/devam");
-ps.writeFileSync("day3/devam/source.txt", "Hello, i am devam");
-var dm=ps.readFileSync("day3/devam/source.txt");
-
-ps.writeFileSync("day3/devam/destination.txt", dm.toString());
