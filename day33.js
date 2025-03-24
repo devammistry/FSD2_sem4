3d3b34ac654f69e31964ffeea2ca5de3eb05ee17
Devam Mistry <devammistry7856@gmail.com>
2025-03-24 20:47:37 +0530

    Delete day33.js

--- day33.js
+++
@@ -1,7 +0,0 @@
-// read data from file & sort that data in ascending order using sort()
-var arr=[10, 20, 30]
-var ps=require("fs");
-ps.writeFileSync("source.txt", arr.toString());
-var dm=ps.readFileSync("source.txt", "utf-8");
-dm_split=dm.split(",")
-console.log(dm_split.sort((a,b)=>a-b));
