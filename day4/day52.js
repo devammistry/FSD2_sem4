cc759223648848842b942550fa644a5a5ba4c751
Devam Mistry <devammistry7856@gmail.com>
2025-03-27 13:39:06 +0530

    Add files via upload

---
+++ day4/day52.js
@@ -0,0 +1,28 @@
+var fs=require("fs");
+fs.writeFile("tuv.txt", "Hello", function(err) {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log("written");
+    }
+}
+);
+
+fs.appendFile("tuv.txt", "Hi", function(err) {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log("appended");
+    }
+});
+
+data=fs.readFile("tuv.txt", "utf-8", function(readerr, data) {
+    if(readerr) {
+        console.log(readerr);
+    }
+    else {
+        console.log(data);
+    }
+})
