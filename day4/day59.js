83c144095b82e031b4529c05a9055472c8051cf0
Devam Mistry <devammistry7856@gmail.com>
2025-03-27 13:42:26 +0530

    Add files via upload

---
+++ day4/day59.js
@@ -0,0 +1,9 @@
+// HTTP Module
+
+var http = require("http");
+//create a server object:
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+}).listen(7856); //Server listening on port 
