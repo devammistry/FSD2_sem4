fced7a843771233ef06a9ffdf00de7f186e4d790
Devam Mistry <devammistry7856@gmail.com>
2025-03-27 13:41:09 +0530

    Add files via upload

---
+++ day4/day56.js
@@ -0,0 +1,9 @@
+var pm=require("path");
+path=pm.dirname("D:/LJ/ktm.html");
+console.log(path);
+path=pm.basename("D:/LJ/ktm.html");
+console.log(path);
+ext=pm.extname("D:/LJ/ktm.html");
+console.log(ext);
+path=pm.parse("D:/LJ/ktm.html");
+console.log(path);
