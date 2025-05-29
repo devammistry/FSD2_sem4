9e4547a1ac1ce89b19625afe6651471bc9ffc265
Devam Mistry <devammistry7856@gmail.com>
2025-05-29 13:27:58 +0530

    Add files via upload

---
+++ T3/Map1.js
@@ -0,0 +1,11 @@
+function Map1() {
+  const arr=[1,2,3,4,5,6,7,8]
+  return(
+    <div>
+        {
+            arr.map((value)=>{return <h2>{value}</h2>})
+        }
+    </div>
+  )
+}
+export default Map1
