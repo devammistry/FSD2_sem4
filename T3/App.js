b42a6157b4d5e68354f722da1762f5587b0d181a
Devam Mistry <devammistry7856@gmail.com>
2025-05-29 13:29:25 +0530

    Add files via upload

---
+++ T3/App.js
@@ -0,0 +1,62 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+//     //       Learn React
+//     //     </a>
+//     //   </header>
+//     // </div>
+//     <div>
+//       <p>
+//         Hello World!
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import Text from "./Text.js"
+
+// function App() {
+//   return(
+//     <div>
+//       <Text/>
+//     </div>
+//   )
+// }
+// export default App
+
+// import Text1 from "./Text1.js"
+
+// function App() {
+//   return(
+//     <div>
+//       <Text1/>
+//     </div>
+//   )
+// }
+// export default App
+
+import Map1 from "./Filt1.js"
+
+function App() {
+  return(
+      <Map1/>
+  )
+}
+export default App
